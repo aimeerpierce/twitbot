e78c0bab631f17c14e6ceee012487e47f005889f
Aimee Pierce <aimee@gforgegroup.com>
2016-09-05 18:56:52 -0500

    Added Tuiter, modified package.json

--- index2.js
+++ index2.js
@@ -29,7 +29,7 @@ var config = {
 function getListMembers(callback) {
     var memberIDs = [];
 
-    tu.listMembers({owner_screen_name: config.me,
+    Twitter.listMembers({owner_screen_name: config.me,
         slug: config.myList
     },
     function(error, data){
@@ -74,7 +74,7 @@ function onTweet(tweet) {
         console.log("RT: " + tweet.text);
         // Note we're using the id_str property since javascript is not accurate
         // for 64bit ints.
-        tu.retweet({
+        Twitter.retweet({
             id: tweet.id_str
         }, onReTweet);
     //}
@@ -82,7 +82,7 @@ function onTweet(tweet) {
 
 // Function for listening to twitter streams and retweeting on demand.
 function listen(listMembers) {
-    tu.filter({
+    Twitter.filter({
         follow: listMembers
     }, function(stream) {
         console.log("listening to stream");
@@ -92,13 +92,20 @@ function listen(listMembers) {
 
 // The application itself.
 // Use the tuiter node module to get access to twitter.
-var tu = require('twitter')(config.keys);
-var client = new Twitter({
-	consumer_key:'4pKGaC8RTPbGFFh0tovxpkS9F',
-	consumer_secret: 'SqKWYkwHtz9kg5sk9E7prWI4Eg3h7mfDXktOJIE9CGnDyVW3Nm',
-	access_token_key: '771489853858918400-djkiab7VNgQY4ttdO9HDnS32233zNcD',
-	access_token_secret: 'J9SLNWD7Z5QyiPqnlZfwiCTDQGlDXilgZO3LuStbWcFHT'
-});
+var Twitter = require('tuiter')(config.keys);
+
+// var client = new Twitter({
+// 	consumer_key:'4pKGaC8RTPbGFFh0tovxpkS9F',
+// 	consumer_secret: 'SqKWYkwHtz9kg5sk9E7prWI4Eg3h7mfDXktOJIE9CGnDyVW3Nm',
+// 	access_token_key: '771489853858918400-djkiab7VNgQY4ttdO9HDnS32233zNcD',
+// 	access_token_secret: 'J9SLNWD7Z5QyiPqnlZfwiCTDQGl:DXilgZO3LuStbWcFHT'
+// });
+// var params = {screen_name: 'nodejs'};
+// client.get('statuses/user_timeline', params, function(error, tweets, response) {
+//   if (!error) {
+//     console.log(tweets);
+//   }
+// });
 // Run the application. The callback in getListMembers ensures we get our list
 // of twitter streams before we attempt to listen to them via the twitter API.
 getListMembers(listen);
